ed681e9e6f50efbdf50df29f1cfad11ed1f4340b
Amith Raj <111039672+araj05@users.noreply.github.com>
2022-08-11 11:30:50 +0530

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,41 @@
+function fav1(){
+    document.getElementById("b1").innerHTML = "Added to favourites!";
+    const lis = document.createElement("li");
+    lis.classList.add("list-group-item");
+    lis.textContent = "<img src='book1.jpg'";
+    const l1 = document.getElementById("fav");
+    l1.appendChild(lis);
+}
+function fav2(){
+    document.getElementById("b2").innerHTML = "Added to favourites!";
+}
+function fav3(){
+    document.getElementById("b3").innerHTML = "Added to favourites!";
+}
+function fav4(){
+    document.getElementById("b4").innerHTML = "Added to favourites!";
+}
+function fav5(){
+    document.getElementById("b5").innerHTML = "Added to favourites!";
+}
+function fav6(){
+    document.getElementById("b6").innerHTML = "Added to favourites!";
+}
+function fav7(){
+    document.getElementById("b7").innerHTML = "Added to favourites!";
+}
+function fav8(){
+    document.getElementById("b8").innerHTML = "Added to favourites!";
+}
+function fav9(){
+    document.getElementById("b9").innerHTML = "Added to favourites!";
+}
+function fav10(){
+    document.getElementById("b10").innerHTML = "Added to favourites!";
+}
+function fav11(){
+    document.getElementById("b11").innerHTML = "Added to favourites!";
+}
+function fav12(){
+    document.getElementById("b12").innerHTML = "Added to favourites!";
+}    
